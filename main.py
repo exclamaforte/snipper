7c3f5e91d8e276787a7b2a34a65a2fc2236de1d9
Daniel Daphron Kaczmarek <xenodaphron@gmail.com>
2015-11-21 19:54:16 -0600

    sending data

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import socket
 from threading import Thread
 
+HOST = 'localhost'
+PORT = 53706
+
 def get_input():
-    HOST = 'localhost'
-    PORT = 53706
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
     print('Connected by', addr)
     while True:
         data = conn.recv(1024) #size?
-        conn.sendall(data)
+        if not data: 
+            break
+        # conn.sendall(data)
         recieve(data)
     conn.close()
 
 def recieve(str_in):
     print(str_in)
+    send_result(str_in)
+
+def send_result(str_out):
+    conn.sendall(str_out)
+    print("SENT")
 
 if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+
     input_thread = Thread(target = get_input)
     input_thread.start()
     input_thread.join()
